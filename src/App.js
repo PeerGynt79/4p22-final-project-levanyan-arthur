c369443c9b3f7f388dd65d8968e0a342be42c140
PeerGynt79 <79arturl79@gmail.com>
2022-11-16 15:08:21 +0300

    react edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
 import './App.css';
 import MyComponentIndex from './components/MyComp/MyComponentIndex';
-/* это массив с данными его 
-const cardsData = [{
-  name:'Xeon X5675(6 ядер) RAM 16Gb',
-  price: '~5300 р.',
-  url:'https://aliexpress.ru/item/1005004408100576.html?pdp_npi=2%40dis%21RUB%2113%C2%A0038%2C21%20%D1%80%D1%83%D0%B1.%218%C2%A0605%2C46%20%D1%80%D1%83%D0%B1.%21%21%21%21%21%4021135c3016644457684532749e214d%2112000029073506373%21sh&sku_id=12000029073506373&spm=a2g0o.store_pc_allProduct.8148356.12.24de1348k06wO0'
-},
-{ name:'Xeon E5 2660V3(10 ядер) RAM 16Gb</div>',
-  price:'~8600 р.',  
-  url:'https://aliexpress.ru/item/1005004408100576.html?pdp_npi=2%40dis%21RUB%2113%C2%A0038%2C21%20%D1%80%D1%83%D0%B1.%218%C2%A0605%2C46%20%D1%80%D1%83%D0%B1.%21%21%21%21%21%4021135c3016644457684532749e214d%2112000029073506373%21sh&sku_id=12000029073506373&spm=a2g0o.store_pc_allProduct.8148356.12.24de1348k06wO0' 
-},
-{ name: 'Xeon E5 2699V3 (18 ядер) RAM 64Gb',
-  price:'~22000 р.',
-  url:'https://aliexpress.ru/item/1005004477250732.html?_ga=2.132711054.1201226875.1664444854-840663976.1664444854&pdp_npi=2%40dis%21RUB%2131%C2%A0919%2C99%20%D1%80%D1%83%D0%B1.%2122%C2%A0024%2C55%20%D1%80%D1%83%D0%B1.%21%21%21%21%21%40211675d216644459172414489e075a%2112000029300672120%21sh&sku_id=12000029300672120&spm=a2g0o.store_pc_allProduct.8148356.12.747d6d6fKg42OE' 
-},
-{ name:'2*Xeon E5 2699V3 (18 ядер) RAM 128Gb', 
-  price:'~53200 р.',
-  url:'https://aliexpress.ru/item/1005004276396260.html?_ga=2.161597116.1201226875.1664444854-840663976.1664444854&pdp_npi=2%40dis%21RUB%2174%C2%A0410%2C27%20%D1%80%D1%83%D0%B1.%2152%C2%A0831%2C09%20%D1%80%D1%83%D0%B1.%21%21%21%21%21%40211675d216644454127513832e075a%2112000028599684875%21sh&sku_id=12000028599684875&spm=a2g0o.store_pc_allProduct.8148356.2.5a0220a0nqmYhP' 
-}];
-*/
 function App() {
   return (
     <div className='App'>
@@ -26,5 +7,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
